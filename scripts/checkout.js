ff502fd8db208bf5873feabeba3fa20626d703b7
GodstimePeculiar <godstimepeculiar7@gmail.com>
2025-08-07 17:17:59 -0700

    Save cart in LocalStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -95,7 +95,7 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
-      const productId = link.dataset.productId
+      const { productId } = link.dataset
       removeFromCart(productId);
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
